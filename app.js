bc388c3a36f470a5aace2d5d7dc1f24dbfb9d966
Matt Fourie <mattfourie3@gmail.com>
2023-04-11 20:36:34 +0200

    full responsiveness and need to add the scrolling animation and site should be ready to be shown to client

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ dropDownHidden.forEach(Link=>{
 
 // ChatGPT
 // Clicked number and copied
-
-// Need to add this to the contact button on the footer 
+// SideBar
 
 const phoneNumber = document.getElementById('phone_tag');
 
@@ -31,11 +30,12 @@ const phoneNumber = document.getElementById('phone_tag');
 phoneNumber.addEventListener('click', (event) => {
   //Copy the content to the clipboard
   navigator.clipboard.writeText(event.target.textContent).then(() => {
-    console.log('Content copied to clipboard');
+    // console.log('Content copied to clipboard');
   });
 
   //Prevent the page from refreshing
   event.preventDefault();
+
 
 //Show a custom popup message
   const popup = document.createElement('div');
@@ -55,7 +55,43 @@ phoneNumber.addEventListener('click', (event) => {
 });
 
 
-// hide and display text in the services section
+// ChatGPT
+// Clicked number and copied
+// footer contact number
+
+const footerPhoneNumber = document.getElementById('footer_phone_tag');
+
+// Add a click event listener to the element
+footerPhoneNumber.addEventListener('click', (event) => {
+  //Copy the content to the clipboard
+  navigator.clipboard.writeText(event.target.textContent).then(() => {
+    // console.log('Content copied to clipboard');
+  });
+
+  //Prevent the page from refreshing
+  event.preventDefault();
+
+
+//Show a custom popup message
+  const popup = document.createElement('div');
+  popup.innerText = 'Copied!';
+  popup.classList.add('popup');
+  document.body.appendChild(popup);
+
+//Position the popup message relative to the clicked element
+  const rect = event.target.getBoundingClientRect();
+  popup.style.top = rect.top + window.pageYOffset + 'px';
+  popup.style.left = rect.left + window.pageXOffset + 'px';
+
+//Remove the popup message after a few seconds
+  setTimeout(() => {
+    popup.remove();
+  }, 1000);
+});
+
+
+
+// hide and display text button in the services section
 // card01
 const servicesText01 = document.querySelector("#services_content_01");
 const readMore01 = document.querySelector('#read_more_btn_01');
@@ -124,45 +160,3 @@ showLess04.addEventListener("click", ()=> {
   readMore04.classList.toggle('hidden');
 });
 
-// Contact Form
-// ChatGPT
-// https://www.youtube.com/watch?v=dgcYOm8n8ME
-// https://www.emailjs.com/
-
-// const form = document.getElementById('myform');
-
-// form.addEventListener('submit', function(event) {
-//     Prevent the form from submitting
-//     event.preventDefault();
-//     Add validation code here
-//     const nameInput = document.getElementById('name');
-//     const emailInput = document.getElementById('email');
-//     const messageInput = document.getElementById('message');
-
-
-//     if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
-//         alert('Please fill in all fields');
-//         return;
-//         }
-
-//         var params = {
-//             name: document.getElementById("name").value,
-//             email: document.getElementById("email").value,
-//             message: document.getElementById("message").value,
-//         };
-        
-//         const serviceID = "service_fzp5foi";
-//         const templateID = "template_tw7sf1w";
-
-//         emailjs
-//             .send(serviceID, templateID, params)
-//             .then(res=>{
-//             document.getElementById("name").value = "";
-//             document.getElementById("email").value = "";
-//             document.getElementById("message").value = "";
-//               console.log(res);
-//             alert("Your message sent successfully!!")
-//         })
-//         .catch(err=>console.log(err));
-
-//     });
